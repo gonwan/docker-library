2f0dd8db5bfc819d0bdf6f9ff400abf4eab8617c
Binhao Qian <gonwan@gmail.com>
2020-12-09 21:05:52 +0800

    simplify scripts.

--- mysql-master-slave/scripts/consul-template-start.sh
+++ mysql-master-slave/scripts/consul-template-start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 apk add openrc haproxy            \
-  && mkdir -p /run/openrc         \
-  && touch /run/openrc/softlevel  \
   && rc-status                    \
+  && touch /run/openrc/softlevel  \
   && rc-service haproxy start
 consul-template -consul-addr "172.16.111.110:8500" -template "/consul-template/data/haproxy.cfg.ctmpl:/etc/haproxy/haproxy.cfg:rc-service haproxy reload"
