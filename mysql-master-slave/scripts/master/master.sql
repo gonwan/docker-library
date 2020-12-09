ad26a97e1d4c2b1f408ce7b959d31634bd2e9c57
Binhao Qian <gonwan@gmail.com>
2020-12-09 10:15:41 +0800

    use sqlite backend.

--- mysql-master-slave/scripts/master/master.sql
+++ mysql-master-slave/scripts/master/master.sql
@@ -2,10 +2,6 @@
 CREATE USER 'repl'@'%' IDENTIFIED BY 'repl';
 GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
 -- See: https://github.com/openark/orchestrator/blob/master/docs/install.md
--- backend
-CREATE DATABASE IF NOT EXISTS orchestrator;
-CREATE USER 'orchestrator'@'%' IDENTIFIED BY 'orch_backend_password';
-GRANT ALL PRIVILEGES ON `orchestrator`.* TO 'orchestrator'@'%';
 -- topology
 CREATE USER 'orc'@'%' IDENTIFIED BY 'orc';
 GRANT SUPER, PROCESS, REPLICATION SLAVE, RELOAD ON *.* TO 'orc'@'%';
