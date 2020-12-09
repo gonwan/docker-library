7fa29a7a21c357b44c4f1a3a091d0c0b61a4d9b6
Binhao Qian <gonwan@gmail.com>
2020-12-09 15:16:21 +0800

    fix cluster alias and add discovery seeds.

--- mysql-master-slave/scripts/slave/slave.sql
+++ mysql-master-slave/scripts/slave/slave.sql
@@ -1,3 +1,5 @@
+-- Wait for master.
+SELECT SLEEP(5);
 -- Note: master_host is hardcoded here.
 CHANGE MASTER TO master_host='172.16.111.10', master_port=3306, master_user='repl', master_password='repl', MASTER_AUTO_POSITION=1;
 START SLAVE;
