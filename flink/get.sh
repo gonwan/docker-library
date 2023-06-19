08b8930259f6bbd46b5bc803c9fe04a281d8a469
Binhao Qian <qianbinhao@eastmoney.com>
2023-06-19 11:24:54 +0800

    update flink.
    Former-commit-id: e9c227050c9917627bd0753b1b3aa32c5f2f7473 [formerly 9f5a965b10515a915b0c2b24bf456829b068ac81]
    Former-commit-id: b306c3432bfa2c5ad219ff1b8a1c47f75f298da8

---
+++ flink/get.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sex -x
+wget https://repo1.maven.org/maven2/org/apache/flink/flink-sql-connector-elasticsearch7/1.16.2/flink-sql-connector-elasticsearch7-1.16.2.jar -O lib/flink-sql-connector-elasticsearch7-1.16.2.jar
+wget https://repo1.maven.org/maven2/com/ververica/flink-sql-connector-mysql-cdc/2.3.0/flink-sql-connector-mysql-cdc-2.3.0.jar -O lib/flink-sql-connector-mysql-cdc-2.3.0.jar
